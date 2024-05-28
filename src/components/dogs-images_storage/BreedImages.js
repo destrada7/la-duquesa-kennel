2340d8a4fe9c416cf92ec6950412371b0502789c
David Estrada <113490499+destrada7@users.noreply.github.com>
2024-05-28 13:34:56 -0500

    Dogs images added

--- src/components/dogs-images_storage/BreedImages.js
+++ src/components/dogs-images_storage/BreedImages.js
@@ -25,25 +25,75 @@ const BreedImages = {
 
     ],
     dalmatian: [
-
+        require("../../images/dogs/dalmatian/dalmatian_1.jpg"),
+        require("../../images/dogs/dalmatian/dalmatian_2.jpg"),
+        require("../../images/dogs/dalmatian/dalmatian_3.jpg"),
+        require("../../images/dogs/dalmatian/dalmatian_4.jpg"),
+        require("../../images/dogs/dalmatian/dalmatian_5.jpg"),
+        require("../../images/dogs/dalmatian/dalmatian_6.jpg"),
+        require("../../images/dogs/dalmatian/dalmatian_7.jpg"),
+        require("../../images/dogs/dalmatian/dalmatian_8.jpg"),
+        require("../../images/dogs/dalmatian/dalmatian_9.jpg"),
+        require("../../images/dogs/dalmatian/dalmatian_10.jpg")
     ],
     bostonTerrier: [
+        require("../../images/dogs/boston-terrier/boston_1.jpg"),
+        require("../../images/dogs/boston-terrier/boston_2.jpg"),
+        require("../../images/dogs/boston-terrier/boston_3.jpg"),
+        require("../../images/dogs/boston-terrier/boston_4.jpg"),
+        require("../../images/dogs/boston-terrier/boston_5.jpg"),
+        require("../../images/dogs/boston-terrier/boston_6.jpg"),
+        require("../../images/dogs/boston-terrier/boston_7.jpg"),
+        require("../../images/dogs/boston-terrier/boston_8.jpg"),
+        require("../../images/dogs/boston-terrier/boston_9.jpg"),
+        require("../../images/dogs/boston-terrier/boston_10.jpg")
 
     ],
     shihTzu: [
+        require("../../images/dogs/shihtzu/stzu_1.jpg"),
+        require("../../images/dogs/shihtzu/stzu_2.jpg"),
+        require("../../images/dogs/shihtzu/stzu_3.jpg"),
+        require("../../images/dogs/shihtzu/stzu_4.jpg"),
+        require("../../images/dogs/shihtzu/stzu_5.jpg"),
 
     ],
     beagle: [
+        require("../../images/dogs/beagle/beagle_1.jpg"),
+        require("../../images/dogs/beagle/beagle_2.jpg"),
+        require("../../images/dogs/beagle/beagle_3.jpg"),
+        require("../../images/dogs/beagle/beagle_4.jpg"),
+        require("../../images/dogs/beagle/beagle_5.jpg"),
+        require("../../images/dogs/beagle/beagle_6.jpg"),
 
     ],
     pug: [
-
+        require("../../images/dogs/pug/pug_1.jpg"),
+        require("../../images/dogs/pug/pug_2.jpg"),
+        require("../../images/dogs/pug/pug_3.jpg"),
+        require("../../images/dogs/pug/pug_4.jpg"),
+        require("../../images/dogs/pug/pug_5.jpg"),
+        require("../../images/dogs/pug/pug_6.jpg"),
+        require("../../images/dogs/pug/pug_7.jpg"),
     ],
     pomerania: [
+        require("../../images/dogs/pomeranian/pom_1.jpg"),
+        require("../../images/dogs/pomeranian/pom_2.jpg"),
+        require("../../images/dogs/pomeranian/pom_3.jpg"),
+        require("../../images/dogs/pomeranian/pom_4.jpg"),
+        require("../../images/dogs/pomeranian/pom_5.jpg"),
 
     ],
     doberman: [
-
+        require("../../images/dogs/doberman/dob_1.jpg"),
+        require("../../images/dogs/doberman/dob_2.jpg"),
+        require("../../images/dogs/doberman/dob_3.jpg"),
+        require("../../images/dogs/doberman/dob_4.jpg"),
+        require("../../images/dogs/doberman/dob_5.jpg"),
+        require("../../images/dogs/doberman/dob_6.jpg"),
+        require("../../images/dogs/doberman/dob_7.jpg"),
+        require("../../images/dogs/doberman/dob_8.jpg"),
+        require("../../images/dogs/doberman/dob_9.jpg"),
+        require("../../images/dogs/doberman/dob_10.jpg"),
     ],
 };
 
